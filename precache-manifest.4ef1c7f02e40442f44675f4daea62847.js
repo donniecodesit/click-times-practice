31aa0e75568a011f54b74dd704a23f6422f5fd81
Donovan Laws <donovan.laws@gmail.com>
2021-12-16 13:34:14 -0700

    Updates

---
+++ precache-manifest.4ef1c7f02e40442f44675f4daea62847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8879b4a0a0a5f177abdd1e37533f8c",
+    "url": "/click-times-practice/index.html"
+  },
+  {
+    "revision": "687845c7c88ccf026275",
+    "url": "/click-times-practice/static/js/2.807484c3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/click-times-practice/static/js/2.807484c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c360c8eb9cbef5c7b88",
+    "url": "/click-times-practice/static/js/main.26740970.chunk.js"
+  },
+  {
+    "revision": "5ff3e16aef2e639a7d57",
+    "url": "/click-times-practice/static/js/runtime-main.62c1dd5a.js"
+  }
+]);
